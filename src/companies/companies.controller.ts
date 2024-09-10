4f445d7a1b62c2ad594e182cc24b25b28a04b908
= <=>
2024-09-10 18:11:49 -0500

    added endpoint and fixed errors due to schema

---
+++ src/companies/companies.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CompaniesService } from './companies.service';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+
+@Controller('companies')
+export class CompaniesController {
+  constructor(private readonly companyService: CompaniesService ) {}
+
+  @Post()
+  create(@Body() createCompanyDto: CreateCompanyDto) {
+    return this.companyService.create(createCompanyDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.companyService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.companyService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
+    return this.companyService.update(id, updateCompanyDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.companyService.remove(id);
+  }
+  
+}
