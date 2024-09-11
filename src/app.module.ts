2965dfe7976765ce254e7bbf4a30775ea4792fae
Mahi_G13 <73084766+MahicolGallego@users.noreply.github.com>
2024-09-11 17:50:55 -0400

    Add Several global configurations and utils (#4)
    * Add error manager - Working in Auth
    
    * WIP: several general settings and company feature
    
    * Add cors configurations
    
    * Add Several global configurations and utils (Remove Auth files)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { TasksModule } from './tasks/tasks.module';
 import { CompaniesModule } from './companies/companies.module';
 import { PrismaServiceModule } from './prisma-service/prisma-service.module';
 
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TasksModule, CompaniesModule, PrismaServiceModule],
+<<<<<<< HEAD
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TasksModule,
+    CompaniesModule,
+    PrismaServiceModule,
+  ],
+=======
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TasksModule],
+>>>>>>> 925861f (Add Several global configurations and utils (Remove Auth files))
   controllers: [],
-  
 })
 export class AppModule {}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
