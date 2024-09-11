de32bd0f4f724702285668ee887bfa709ade893b
Mahi_G13 <maicolgallego14@gmail.com>
2024-09-11 12:14:34 -0500

    Add Several global configurations and utils (Remove Auth files)

--- src/common/interfaces/auth/payload-token.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Roles } from "src/common/constants/enum-roles";
-
-export interface PayloadToken{
-    sub: string,
-    role: Roles
-}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { Request } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/v1');
 
   //Configure cors options
   const corsOptionsDelegate = (req: Request, callback: any) => {
@@ -37,6 +38,17 @@ async function bootstrap() {
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
 
+  //Config Swagger to API Documentation
+  const config = new DocumentBuilder()
+    .setTitle('ColLaborat API')
+    .setDescription(
+      'CollaboraT is a robust platform designed for companies to manage their projects efficiently and collaboratively. Inspired by tools like Jira, our system provides a comprehensive solution for planning, monitoring and executing projects, adapting to the specific needs of each organization.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('collaborat/api/v1/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
