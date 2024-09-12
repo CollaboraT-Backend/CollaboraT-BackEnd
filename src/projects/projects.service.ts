26c6786cf44a6a5076a7aab55b87bfe3bcc46c9a
ElverTunubala <elvertunubala@gmail.com>
2024-09-11 20:43:45 -0500

    add ErrorManager in project service

---
+++ src/projects/projects.service.ts
@@ -0,0 +1,113 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+import { PrismaService } from 'src/prisma-service/prisma-service.service';
+import { Project, ProjectStatus } from '@prisma/client';
+import { ErrorManager } from 'src/common/filters/error-manager.filter';
+
+@Injectable()
+export class ProjectsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createProjectDto: CreateProjectDto) {
+    try {
+      return await this.prisma.project.create({ data: createProjectDto });
+    } catch (error) {
+      if (error instanceof Error) {
+        throw ErrorManager.createSignatureError(
+          `INTERNAL_SERVER_ERROR :: ${error.message}`,
+        );
+      }
+      throw ErrorManager.createSignatureError('An unexpected error ocurred');
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.prisma.project.findMany();
+    } catch (error) {
+      if (error instanceof Error) {
+        throw ErrorManager.createSignatureError(
+          `INTERNAL_SERVER_ERROR :: ${error.message}`,
+        );
+      }
+      throw ErrorManager.createSignatureError('An unexpected error ocurred');
+    }
+  }
+
+  async findOne(id: string) {
+    try {
+      const project = await this.prisma.project.findFirst({
+        where: {
+          id: id,
+          status: {
+            not: 'archived',
+          },
+        },
+      });
+
+      if (!project) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Project not found',
+        });
+      }
+
+      return project;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw ErrorManager.createSignatureError(error.message);
+      }
+      throw ErrorManager.createSignatureError('An unexpected error ocurred');
+    }
+  }
+
+  async update(id: string, updateProjectDto: UpdateProjectDto) {
+    try {
+      const updatedProject = await this.prisma.project.update({
+        where: { id },
+        data: updateProjectDto,
+      });
+
+      if (!updatedProject) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Project not found',
+        });
+      }
+
+      return updatedProject;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw ErrorManager.createSignatureError(error.message);
+      }
+      throw ErrorManager.createSignatureError('An unexpected error ocurred');
+    }
+  }
+
+  async remove(id: string) {
+    try {
+      const projectDeleted = await this.prisma.project.update({
+        where: { id },
+        data: { status: ProjectStatus.archived, deletedAt: new Date() },
+      });
+
+      if (!projectDeleted) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Project not found',
+        });
+      }
+
+      return {
+        success: true,
+        message: 'Project deleted successfully',
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        throw ErrorManager.createSignatureError(error.message);
+      }
+      throw ErrorManager.createSignatureError('An unexpected error ocurred');
+    }
+  }
+}
